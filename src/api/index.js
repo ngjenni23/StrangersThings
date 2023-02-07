17f1e3b9c1503b09e30425653d0b5b6c009977d9
“Jennifer <ngjenni23@gmail.com>
2023-02-06 21:11:09 -0500

    project init

---
+++ src/api/index.js
@@ -0,0 +1,3 @@
+const cohortName = '2211-ftb-et-web-pt';
+export const BASE_URL = `https://strangers-things.herokuapp.com/api/${cohortName}`;
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
